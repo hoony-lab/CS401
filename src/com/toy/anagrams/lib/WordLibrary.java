5701b48e82d4a5235fd2cff51f9cf750bcafa423
COSLAB <COSLAB@A116882.ad.csueastbay.edu>
2018-10-10 12:34:42 -0700

    commit ss

--- src/com/toy/anagrams/lib/WordLibrary.java
+++ src/com/toy/anagrams/lib/WordLibrary.java
@@ -48,31 +48,4 @@ public abstract class WordLibrary {
         return StaticWordLibrary.DEFAULT;
     }
 
-    /**
-     * Gets the word at a given index.
-     * @param idx index of required word
-     * @return word at that index in its natural form
-     */
-    public abstract String getWord(int idx);
-
-    /**
-     * Gets the word at a given index in its scrambled form.
-     * @param idx index of required word
-     * @return word at that index in its scrambled form
-     */
-    public abstract String getScrambledWord(int idx);
-
-    /**
-     * Gets the number of words in the library.
-     * @return the total number of plain/scrambled word pairs in the library
-     */
-    public abstract int getSize();
-
-    /**
-     * Checks whether a user's guess for a word at the given index is correct.
-     * @param idx index of the word guessed
-     * @param userGuess the user's guess for the actual word
-     * @return true if the guess was correct; false otherwise
-     */
-    public abstract boolean isCorrect(int idx, String userGuess);
 }
