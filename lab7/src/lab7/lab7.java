9e07d8c5824ad709bb2ea4bc4c64695d76f8a533
jihun <jihun@Zi-Hoony>
2018-10-11 04:47:03 +0900

    1st

---
+++ lab7/src/lab7/lab7.java
@@ -0,0 +1,12 @@
+package lab7;
+
+public class lab7 {
+
+	public lab7() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String []args) {
+		System.out.println("SPEAKING BAD WORDS ****");
+	}
+}
